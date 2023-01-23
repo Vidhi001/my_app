d81712a362a36f628459020a10f2196e4413d3ce
Aman <apple@APPLEs-MacBook-Air.local>
2023-01-23 22:26:47 +0530

    watch selection fixed

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     title: 'FitBit 19 - The Smartest Watch',
     description: 'Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor.',
     colorOptions: [
-        {   
+        {
             id:1,
             styleName: 'Black Strap',
             imageUrl: 'https://imgur.com/iOeUBV7.png',
@@ -42,6 +42,8 @@ function App() {
   timeVariable = today.getHours() + ':' + today.getMinutes();
   // const prevRef = 0;
 
+    const [selectedWatch,setSelectedWatch] = useState(0);
+
   return (
     <div className="App">
       <div className="row">
@@ -50,17 +52,17 @@ function App() {
       <div className="row main_container">
         <div className = "col-6 leftPage">
           <p>{timeVariable}</p>
-          <img src={ProductData.colorOptions[0].imageUrl} alt ={ProductData.colorOptions[0].styleName}/>
+          <img src={ProductData.colorOptions[selectedWatch].imageUrl} alt ={ProductData.colorOptions[selectedWatch].styleName}/>
         </div>
         <div className = "col-6 rightPage">
           <h2>{ProductData.title}</h2>
           <p>{ProductData.description}</p>
           <div style={{marginBottom: "20px"}}>
             <h4>Select Color</h4>
-            <WatchCard watch_img={ProductData.colorOptions[0]} prevRef={ProductData.colorOptions[0].id}/>
-            <WatchCard watch_img={ProductData.colorOptions[1]} prevRef={ProductData.colorOptions[1].id}/>
-            <WatchCard watch_img={ProductData.colorOptions[2]} prevRef={ProductData.colorOptions[2].id}/>
-            <WatchCard watch_img={ProductData.colorOptions[3]} prevRef={ProductData.colorOptions[3].id}/>
+            <WatchCard index={0} isSelected={selectedWatch==0} setSelectedWatch={setSelectedWatch} watch_img={ProductData.colorOptions[0]} prevRef={ProductData.colorOptions[0].id}/>
+            <WatchCard index={1} isSelected={selectedWatch==1} setSelectedWatch={setSelectedWatch} watch_img={ProductData.colorOptions[1]} prevRef={ProductData.colorOptions[1].id}/>
+            <WatchCard index={2} isSelected={selectedWatch==2} setSelectedWatch={setSelectedWatch} watch_img={ProductData.colorOptions[2]} prevRef={ProductData.colorOptions[2].id}/>
+            <WatchCard index={3} isSelected={selectedWatch==3} setSelectedWatch={setSelectedWatch} watch_img={ProductData.colorOptions[3]} prevRef={ProductData.colorOptions[3].id}/>
           </div>
 
           <h4>Features</h4>
