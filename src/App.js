13be675ff48cce74a3e84bc4297a4599ec64e367
Vidhi001 <vidhimittal296@gmail.com>
2023-01-24 23:09:42 +0530

    functioning added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React , {useState} from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import WatchCard from './WatchCard';
+// import {IonIcon} from "react-ion-icon";
 
+function timeFeature() {
+  var today = new Date(),
+  hours = today.getHours()>9 ? today.getHours() : '0'+today.getHours(),
+  minutes = today.getMinutes()>9 ? today.getMinutes() : '0'+today.getMinutes(),
+  timeVariable = hours + ':' + minutes;
+
+  return (
+    <div className='featureData'>
+      <p>{timeVariable}</p>
+    </div>
+  );
+}
+
+function rateFeature() {
+  const pulseRate = 78;
+  
+  return (
+    <div className='featureData' style={{padding: "16% 22.5%"}}>
+      <i className="fa fa-heartbeat" aria-hidden="true"></i>
+      <p>{pulseRate}</p>
+    </div>
+  );
+}
 
 function App() {
   const ProductData = {
@@ -10,39 +34,29 @@ function App() {
     description: 'Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor.',
     colorOptions: [
         {
-            id:1,
             styleName: 'Black Strap',
-            imageUrl: 'https://imgur.com/iOeUBV7.png',
-            img_state : true
+            imageUrl: 'https://imgur.com/iOeUBV7.png'
         },
         {
-            id:2,
             styleName: 'Red Strap',
-            imageUrl: 'https://imgur.com/PTgQlim.png',
-            img_state : false
+            imageUrl: 'https://imgur.com/PTgQlim.png'
         },
-        {
-            id:3,
+        { 
             styleName: 'Blue Strap',
-            imageUrl: 'https://imgur.com/Mplj1YR.png',
-            img_state : false
+            imageUrl: 'https://imgur.com/Mplj1YR.png'
         },
         {
-            id:4,
             styleName: 'Purple Strap',
-            imageUrl: 'https://imgur.com/xSIK4M8.png',
-            img_state : false
+            imageUrl: 'https://imgur.com/xSIK4M8.png'
         },
     ],
     featureList: [
         "Time", "Heart Rate"
     ]
   }
-  var today = new Date(),
-  timeVariable = today.getHours() + ':' + today.getMinutes();
-  // const prevRef = 0;
 
-    const [selectedWatch,setSelectedWatch] = useState(0);
+  const [selectedWatch,setSelectedWatch] = useState(0);
+  const [selectedfeature,setSelectedFeature] = useState(0);
 
   return (
     <div className="App">
@@ -51,7 +65,9 @@ function App() {
       </div>
       <div className="row main_container">
         <div className = "col-6 leftPage">
-          <p>{timeVariable}</p>
+
+          {selectedfeature===0 ? timeFeature() : rateFeature() }
+
           <img src={ProductData.colorOptions[selectedWatch].imageUrl} alt ={ProductData.colorOptions[selectedWatch].styleName}/>
         </div>
         <div className = "col-6 rightPage">
@@ -59,16 +75,23 @@ function App() {
           <p>{ProductData.description}</p>
           <div style={{marginBottom: "20px"}}>
             <h4>Select Color</h4>
-            <WatchCard index={0} isSelected={selectedWatch==0} setSelectedWatch={setSelectedWatch} watch_img={ProductData.colorOptions[0]} prevRef={ProductData.colorOptions[0].id}/>
-            <WatchCard index={1} isSelected={selectedWatch==1} setSelectedWatch={setSelectedWatch} watch_img={ProductData.colorOptions[1]} prevRef={ProductData.colorOptions[1].id}/>
-            <WatchCard index={2} isSelected={selectedWatch==2} setSelectedWatch={setSelectedWatch} watch_img={ProductData.colorOptions[2]} prevRef={ProductData.colorOptions[2].id}/>
-            <WatchCard index={3} isSelected={selectedWatch==3} setSelectedWatch={setSelectedWatch} watch_img={ProductData.colorOptions[3]} prevRef={ProductData.colorOptions[3].id}/>
+            <WatchCard index={0} isSelected={selectedWatch===0} setSelectedWatch={setSelectedWatch} watch_img={ProductData.colorOptions[0]}/>
+            <WatchCard index={1} isSelected={selectedWatch===1} setSelectedWatch={setSelectedWatch} watch_img={ProductData.colorOptions[1]} />
+            <WatchCard index={2} isSelected={selectedWatch===2} setSelectedWatch={setSelectedWatch} watch_img={ProductData.colorOptions[2]}/>
+            <WatchCard index={3} isSelected={selectedWatch===3} setSelectedWatch={setSelectedWatch} watch_img={ProductData.colorOptions[3]} />
           </div>
 
           <h4>Features</h4>
           <div style={{marginBottom: "20px"}}>
-          <button className= 'btn btn_style btnSelected'>{ProductData.featureList[0]}</button>
-          <button className='btn btn_style'>{ProductData.featureList[1]}</button>
+            <button onClick = {()=> setSelectedFeature(0)}
+              className= {selectedfeature===0 ? 'btn btn_style btnSelected' : 'btn btn_style'}>
+              {ProductData.featureList[0]}
+            </button>
+
+            <button onClick = {()=> setSelectedFeature(1)}
+              className= {selectedfeature===1? 'btn btn_style btnSelected' : 'btn btn_style' }>
+              {ProductData.featureList[1]}
+            </button>
           </div>
 
           <button className='btn btn-primary'>Buy Now</button>
--- src/WatchCard.js
+++ src/WatchCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import "./WatchCard.css"
 
 export default function WatchCard(props) {
-    let {index, isSelected, watch_img, prevRef, setSelectedWatch} = props
+    let {index, isSelected, watch_img, setSelectedWatch} = props;
     return (
         <img
             src={watch_img.imageUrl}
